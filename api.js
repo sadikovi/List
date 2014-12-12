06e86c4da4b51afee2bb96afac482db37c4a30e7
sadikovi <sadikovi@Ivans-MacBook-Pro.local>
2014-12-13 10:28:09 +1300

    refactoring and updating to using threads

--- api.js
+++ api.js
@@ -1,17 +1,39 @@
+var API_SYS_LABELS = {
+    Inbox: "INBOX",
+    Unread: "UNREAD"
+};
+
+var API_TABS = {
+    Important: "IMPORTANT",
+    Personal: "CATEGORY_PERSONAL",
+    Social: "CATEGORY_SOCIAL",
+    Promotions: "CATEGORY_PROMOTIONS"
+};
+
 var API = (function() {
     var messagesURL = "https://www.googleapis.com/gmail/v1/users/me/messages";
+    var threadsURL = "https://www.googleapis.com/gmail/v1/users/me/threads";
 
     return {
-        getInboxUnreadMail: function(access_token, maxResults, callback) {
-            var url = OAuthScheme.addURLParam(messagesURL, "access_token", access_token);
+        getInboxUnreadMailThreads: function(access_token, maxResults, callback) {
+            var url = OAuthScheme.addURLParam(threadsURL, "access_token", access_token);
             url = OAuthScheme.addURLParam(url, "maxResults", maxResults);
-            url = OAuthScheme.addURLParam(url, "labelIds", "INBOX");
-            url = OAuthScheme.addURLParam(url, "labelIds", "UNREAD");
+            url = OAuthScheme.addURLParam(url, "fields", "resultSizeEstimate,threads/id");
+            url = API.searchTabs(url, [API_SYS_LABELS.Inbox, API_SYS_LABELS.Unread]);
             OAuthScheme.sendRequest("GET", url, {}, "", function(xhr, data) {
                 if (xhr.readyState == 4) {
                     callback.call(this, xhr, data);
                 }
             });
+        },
+
+        searchTabs: function(url, tabs) {
+            if (tabs && tabs.length > 0) {
+                for (var i=0; i<tabs.length; i++) {
+                    url = OAuthScheme.addURLParam(url, "labelIds", tabs[i]);
+                }
+            }
+            return url;
         }
     }
 })();
--- background.js
+++ background.js
@@ -3,129 +3,166 @@ var oauth = {
     exchange_url: 'https://www.googleapis.com/oauth2/v3/token',
     client_id: '899698234228-oglo18hbuvmardiq25b5saode9gotsdd.apps.googleusercontent.com',
     client_secret: 'Z6qHj8YGshHKpGV7kaUydit_',
-    scope: 'https://www.googleapis.com/auth/gmail.modify',
+    scope: 'https://www.googleapis.com/auth/gmail.readonly',
     response_type: 'code',
     redirect_uri: 'http://localhost/'
 };
 
-function constructRequestURL(params) {
-    var url = params.request_url;
-    url = OAuthScheme.addURLParam(url, "redirect_uri", params.redirect_uri);
-    url = OAuthScheme.addURLParam(url, "client_id", params.client_id);
-    url = OAuthScheme.addURLParam(url, "scope", params.scope);
-    url = OAuthScheme.addURLParam(url, "response_type", params.response_type);
-    return url;
-}
+var List = (function() {
+    return {
+        constructRequestURL: function(params) {
+            var url = params.request_url;
+            url = OAuthScheme.addURLParam(url, "redirect_uri", params.redirect_uri);
+            url = OAuthScheme.addURLParam(url, "client_id", params.client_id);
+            url = OAuthScheme.addURLParam(url, "scope", params.scope);
+            url = OAuthScheme.addURLParam(url, "response_type", params.response_type);
+            return url;
+        },
 
-function getAccessToken(isRefresh, url, code, params, onAccessToken) {
-    // prepare request
-    var headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    };
+        setIcon: function(isAuth) {
+            var image = (isAuth)?"icon19.png": "icon38-off.png";
+            chrome.browserAction.setIcon({"path":image}, function(){});
+        },
 
-    var body = {
-        "client_id": params.client_id,
-        "client_secret": params.client_secret
-    };
-
-    if (isRefresh) {
-        body["refresh_token"] = code;
-        body["grant_type"] = "refresh_token";
-    } else {
-        body["code"] = code;
-        body["grant_type"] = "authorization_code";
-        body["redirect_uri"] = params.redirect_uri;
-    }
-
-    OAuthScheme.sendRequest("POST", url, headers, OAuthScheme.formEncode(body), onAccessToken);
-}
-
-function onAccessToken(xhr, data) {
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            var res = JSON.parse(xhr.responseText);
-            TokenStore.setAccessToken(res.access_token, oauth.client_id, oauth.scope);
-            TokenStore.setRefreshToken(res.refresh_token, oauth.client_id, oauth.scope);
-            var access_token = TokenStore.getAccessToken(oauth.client_id, oauth.scope);
-            setIcon(true);
-            checkUnreadMessages(access_token);
-        }
-    }
-}
-
-function setIcon(isAuth) {
-    var image = (isAuth)?"icon19.png": "icon38-off.png";
-    chrome.browserAction.setIcon({"path":image}, function(){});
-}
-
-function setBadge(text) {
-    chrome.browserAction.setBadgeText({
-        "text": text
-    });
-}
-
-function deauthorize() {
-    TokenStore.clearTokens(oauth.client_id, oauth.scope);
-}
-
-function authorize() {
-    var url = constructRequestURL(oauth);
-    var tabs = {};
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        // check only when tab is the same and url matches redirect url from oauth
-        if (tabs["active"] && tabs["active"] == tab.id && changeInfo.url && OAuthScheme.getUrl(changeInfo.url) == oauth.redirect_uri) {
-            var decoded = OAuthScheme.getQueryStringParams(changeInfo.url);
-            if (decoded["code"] && !decoded["error"]) {
-                getAccessToken(false, oauth.exchange_url, decoded["code"], oauth, onAccessToken);
+        setBadge: function(text, color) {
+            chrome.browserAction.setBadgeText({ "text": text });
+            if (color) {
+                chrome.browserAction.setBadgeBackgroundColor({ "color": color });
             }
-            chrome.tabs.remove(tab.id);
+        },
+
+        authorize: function(success, error) {
+            if (TokenStore.hasToken(oauth.client_id, oauth.scope)) {
+                var access_token = TokenStore.getAccessToken(oauth.client_id, oauth.scope);
+                success.call(this, access_token);
+            } else {
+                List.initAuthFlow(success, error);
+            }
+        },
+
+        initAuthFlow: function(success, error) {
+            var url = List.constructRequestURL(oauth);
+            var tabs = {};
+            chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+                if (tabs["active"] && tabs["active"] == tab.id && changeInfo.url
+                    && OAuthScheme.getUrl(changeInfo.url) == oauth.redirect_uri) {
+                    var decoded = OAuthScheme.getQueryStringParams(changeInfo.url);
+                    if (decoded["code"] && !decoded["error"]) {
+                        List.getAccessToken(false, oauth.exchange_url, decoded["code"], oauth, success, error);
+                    } else if (!decoded["code"] || decoded["error"]) {
+                        error.call(this, decoded);
+                    }
+                    chrome.tabs.remove(tab.id);
+                }
+            });
+
+            chrome.tabs.create({"url": url}, function(tab) {
+                tabs["active"] = tab.id;
+            });
+        },
+
+        getAccessToken: function(isRefresh, url, code, params, success, error) {
+            var headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            };
+
+            var body = {
+                "client_id": params.client_id,
+                "client_secret": params.client_secret
+            };
+
+            if (isRefresh) {
+                body["refresh_token"] = code;
+                body["grant_type"] = "refresh_token";
+            } else {
+                body["code"] = code;
+                body["grant_type"] = "authorization_code";
+                body["redirect_uri"] = params.redirect_uri;
+            }
+
+            OAuthScheme.sendRequest("POST", url, headers, OAuthScheme.formEncode(body), function(xhr, data) {
+                List.onAccessToken.call(this, xhr, data, success, error);
+            });
+        },
+
+        onAccessToken: function(xhr, data, success, error) {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    var res = JSON.parse(xhr.responseText);
+                    TokenStore.setAccessToken(res.access_token, oauth.client_id, oauth.scope);
+                    TokenStore.setRefreshToken(res.refresh_token, oauth.client_id, oauth.scope);
+                    var access_token = TokenStore.getAccessToken(oauth.client_id, oauth.scope);
+                    success.call(this, access_token);
+                } else {
+                    error.call(this, xhr.responseText);
+                }
+            }
         }
-    });
-
-    chrome.tabs.create({"url": url}, function(tab) {
-        tabs["active"] = tab.id;
-    });
-}
-
-function init() {
-    // check first whether tokens are already stored in the system
-    if (TokenStore.hasToken(oauth.client_id, oauth.scope)) {
-        var access_token = TokenStore.getAccessToken(oauth.client_id, oauth.scope);
-        // set active icon
-        setIcon(true);
-        // check messages
-        checkUnreadMessages(access_token);
-    } else {
-        // otherwise authorize
-        authorize();
     }
-}
+})();
 
-function checkUnreadMessages(access_token) {
-    // lets call api
+/******************************/
+/*******  Start loading *******/
+/******************************/
+window.list_allowRefreshToken = true;
+window.list_allowCheckTimer = true;
+window.list_checkTimer = null;
+window.list_checkTimerInterval = 5*60;
+
+function checkUnreadThreads(access_token) {
     var maxResults = 5;
-    setBadge("...");
-    API.getInboxUnreadMail(access_token, maxResults, function(xhr, data) {
+    List.setBadge("...", "#777");
+    API.getInboxUnreadMailThreads(access_token, maxResults, function(xhr, data) {
         if (xhr.status == 200) {
             var list = JSON.parse(xhr.responseText);
-            if (list["messages"]) {
+            if (list["threads"]) {
                 var estimatedResults = list["resultSizeEstimate"];
-                var listBadge = list["messages"].length;
+                var listBadge = list["threads"].length;
                 if (listBadge > 0) {
-                    setBadge(""+listBadge+((listBadge >= estimatedResults)?"":"+"));
+                    List.setIcon(true);
+                    List.setBadge(""+listBadge+((listBadge >= estimatedResults)?"":"+"), "#333");
                     return false;
                 }
             }
-        } else if (xhr.status == 401) {
-            // access token expired
-            // obtain new access token
-            getAccessToken(true, oauth.exchange_url, TokenStore.getRefreshToken(oauth.client_id, oauth.scope), oauth, onAccessToken);
+        } else if (xhr.status == 401 && window.list_allowRefreshToken) {
+            window.list_allowRefreshToken = false;
+            List.getAccessToken(true, oauth.exchange_url, TokenStore.getRefreshToken(oauth.client_id, oauth.scope), oauth,
+                function(access_token) {
+                    checkUnreadThreads(access_token);
+                    window.list_allowRefreshToken = true;
+                    return false;
+                }
+            );
         }
-
-        setBadge("");
+        List.setBadge("");
     });
 }
 
-setIcon(false);
-setBadge("");
-chrome.browserAction.onClicked.addListener(init);
+// set "off" icon and empty badge
+List.setIcon(false);
+List.setBadge("");
+
+function success(access_token) {
+    List.setIcon(true);
+    checkUnreadThreads(access_token);
+    if (window.list_allowCheckTimer) {
+        window.checkTimer = setInterval(function() {
+            checkUnreadThreads(access_token);
+        }, window.list_checkTimerInterval*1000);
+        window.list_allowCheckTimer = false;
+    }
+}
+
+function error(error) {
+    List.setIcon(false);
+    List.setBadge("");
+    console.log(error);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    List.authorize(success, error);
+});
+
+chrome.browserAction.onClicked.addListener(function() {
+    List.authorize(success, error);
+});
